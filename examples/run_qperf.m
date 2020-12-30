93d8df0fbfc3e63ad53641f2f091a36223319c15
Dimitar Mileski <dime.mileski@hotmail.com>
2020-12-30 20:22:21 +0100

    matlab examples
    Signed-off-by: Dimitar Mileski <dime.mileski@hotmail.com>

---
+++ examples/run_qperf.m
@@ -0,0 +1,25 @@
+% example of QPerf perfusion flow mapping
+
+clear all
+close all
+
+% suppose the QPerf repo was cloned to QPerf directory
+% git clone ...
+
+% load the stored data
+
+%cd .\QPerf\examples
+
+load perf_data_stress
+
+command = ['C:/Users/Dimitar/Downloads/QPerf-master/QPerf-master/software/QPerf/gadgetron_QPerf_mapping -f ./aif_stress -i ./data_stress -m ./MBF_stress --foot ' num2str(foot) ' --dt 500'];
+dos(command);
+
+% load and visualize map
+fmap = analyze75read('MBF_stress');
+
+figure; 
+plot(aif);
+title('AIF');
+
+figure; imshow(fmap, 'DisplayRange', [0 6]);PerfColorMap;
